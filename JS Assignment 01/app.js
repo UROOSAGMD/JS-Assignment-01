5e69ef818c53c13cb8ef6a078bdcb496f2ffe7b5
Uroosa GMD <uroosagmd@gmail.com>
2025-06-03 18:54:10 +0500

    Add files via upload

---
+++ JS Assignment 01/app.js
@@ -0,0 +1,164 @@
+
+                              // Excercies : 01 (Alert) //
+
+        //   Question 1 // 
+       
+        //   1. Write a script to greet your website visitor using JS alert
+        //   box. -->
+        //  alert("welcome my website");
+
+       
+        //   Question 2 //
+
+        //   2. Write a script to display following message on your web
+        //   page:
+        // alert("Warning! Your session will expire soon.");
+
+        //  Question 3 //
+
+        //   3. Write a script to display following message on your web
+        //   page: (Hint : Use line break)
+        //   alert("Hello JavaScript Learner....  \n Keep practicing daily to improve!");
+
+        // Question 4 //
+
+        // 4. Write a script to display following messages in sequence:
+        // alert(" Welcome to Js Land...");
+        // alert("Keep Coding!\nDon’t forget to take short breaks");
+
+        // Question 5 //
+
+        // 5. Generate the following message through browser’s
+        // developer console:
+        // console.log("This message is only visible in the console.");
+
+        //Question 6//
+
+        // 6. Make use of alerts in your new/existing HTML & CSS
+        // project.
+        // alert("Hello! Welcome to the Coding World!");
+
+        // Question  7 //
+
+        // 7. Practice placement of <script></script> element in
+        // following sections of your project in exercise 6:
+        // 1. Head
+        // 2. Body (before your page’s HTML)
+        // 3. Body (inside your page’s HTML)
+        // 4. Body (after your page’s HTML)
+     
+        //   Part (1)  //
+        //<head>
+        //<script>
+            //     alert("Hello! welcome to the Cooding world.");
+        //</script>
+        //</head>
+
+        
+         //   Part (2)   //
+         // <body>
+         //<script>
+         //          alert(" Hello! welcome to the Cooding world.")
+         //</script>
+         //</body>
+
+         // Part (3))
+         //<body>
+         //<h1>Hello!</h1>
+         //<script>
+    
+         //             alert(" Hello! welcome to the Cooding world.")
+         //</script>
+         //<h1>Hello!</h1>
+         //</body>
+
+         //Part (4)
+         //<head>
+         //<script>
+         //          alert(" Hello! welcome to the Cooding world.")
+         //</script>
+         //<h1>Hello!</h1>
+         //</head>
+
+                           //............excersices: 01 # End..........//
+
+
+                           //  Excersices : 02  (Variables For Strings) //
+
+         //  Question 1  //
+
+         // 1. Declare a variable called username.
+        //  var username ;
+
+         //  Question 2 //
+
+         // 2. Declare a variable called myName & assign to it a string
+         // that represents your Full Name.
+         // var  myName = "Full Name";
+
+         //   Question 3  //
+
+         // 3. Write script to
+         // 1) Declare a JS variable, titled message.
+         //    var message;
+         // 2) Assign “Hello World” to variable message
+         //    var message; "Hello world";
+         // 3) Display the message in alert box.
+         //    alert("message");
+
+         //  Question 4  //
+
+         //  4. Write a script to save student’s bio data in JS variables and
+         // show the data in alert boxes.
+         //  var name="Jhone Doe";
+         //  alert(name);
+         //  var age="15 year old";
+         //  alert(age);
+         //  var course="Certified  Mobile Application Development";
+         //  alert(course);
+
+         //   Question 5  //
+
+         //  5. Write a script to display the following alert using one JS
+        //  // variable:
+        //  var message="pizza \n pizz \n piz\n pi\n p";
+        //  alert(message);
+
+         //   Question 6  //
+
+         // Declare a variable called email and assign to it a string that
+         // represents your Email Address(e.g. example@example.com).
+         // Show the blow mentioned message in an alert box.(Hint: use
+         // string concatenation)
+          //  var email = "example@gmail.com";
+          //  alert("My email address is " + email);
+
+          //   Question  7  //
+
+          // 7. Declare a variable called book & give it the value “A
+          // smarter way to learn JavaScript”. Display the following
+          // message in an alert box:
+          // var book = "A smarter way to learn JavaScript";
+          // alert("I am trying to learn from the book" + book);
+
+          //  Question 8  //
+
+          // 8. Write a script to display this in browser through JS
+            //  var num=("Yah! I can write HTML content through JavaScript");
+            //  document.write(num);
+
+          //  Question 9  //
+
+          // 9. Store following string in a variable and show in alert and
+          // browser through JS
+
+          // var message="Hello";
+          // alert(message);
+          // document.write(message);
+
+
+                          //............excersices : 02 # End..........//
+
+
+   
+
